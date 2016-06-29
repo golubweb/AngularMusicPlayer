01f9d48828b61358cbad63578f6526df43bb04ff
golubweb <darko.golubovic@hotmail.rs>
2016-06-29 16:56:46 +0200

    add files

--- app/controllers/mainCtrl.js
+++
@@ -1,9 +0,0 @@
-/***************************
-*
-* @author: Darko Golubovic
-* @date: 27-06-2016
-*
-***************************/
-myApp.controller('mainCtrl', ['$scope', function($scope, mainServices, mainFactory){
-	
-}]);
